26c419f2a113d12e489a4cb2bde0480dc1178dd1
Sharareh <shararehg79@yahoo.com>
2023-12-22 13:34:27 -0800

    removed controllers and routes

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const typeDefs = `
+type Query {
+    me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+  }
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+// like act 26
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(book: BookInput): User
+    deleteBook(bookId: String!): User
+}
+`;
+
+module.exports = typeDefs;
